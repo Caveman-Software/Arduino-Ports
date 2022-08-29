8dcd775b3dfe5c256bb1ccf2e00994a7ab5dcd2d
Michael Mulvey <michael.mulvey@roadrunner.com>
2022-08-29 13:57:38 -0400

    Update ArduinoPorts.py

--- ArduinoPorts.py
+++ ArduinoPorts.py
@@ -16,19 +16,6 @@ except:
 
 root = Tk()
 
-# Add Icon to window Titlebar
-if os.name == 'nt':
-    homepath = os.path.expanduser('~')
-    tempFile = '%s\Caveman Software\%s' % (homepath, 'Icon\icon.ico')
-
-    if (os.path.exists(tempFile) == True):
-        root.wm_iconbitmap(default=tempFile)
-
-    else:
-        import create_icon
-        print('File Created')
-        root.wm_iconbitmap(default=tempFile)
-
 title = (os.path.basename(__file__)[0:-3])
 root.title(title.title())
 root.geometry('400x200')
